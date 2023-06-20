bb2fdf5948d1f350106b0198fecfd417517bb985
Francisco Cruz <franciscohuertas25@gmail.com>
2023-06-20 09:53:02 -0500

    Added openAI key

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
 
-# Create your views here.
+openai_api_key = '' # put your api key here from https://platform.openai.com/account/api-keys
+openai.api_key = openai_api_key
+
+
+def ask_openai(message):
+    respose = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
+
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'Hi this is a response'
+        response = ask_openai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
